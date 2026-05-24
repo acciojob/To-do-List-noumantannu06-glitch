9222ac09d079300ae920a293c5daa95c8392a42a
Md Nouman <noumantannu06@gmail.com>
2026-05-24 23:26:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
+const input = document.getElementById('newTodoInput');
+const addBtn = document.getElementById('addTodoBtn');
+const todoList = document.getElementById('todoList');
+
+addBtn.addEventListener('click', () => {
+  const todoText = input.value.trim();
+
+  if (todoText === '') {
+    return;
+  }
+
+  const li = document.createElement('li');
+  li.textContent = todoText;
+  todoList.appendChild(li);
+
+  input.value = '';
+});
 
